71710916f3b387e41589f53e0cffb59a429d515d
Alec Levin <arl505@humboldt.edu>
2019-08-29 18:12:27 -0400

    added springboot initializer starter

---
+++ src/main/java/org/arlevin/memeDatabaseBot/MemeDatabaseBotApplication.java
@@ -0,0 +1,12 @@
+package org.arlevin.memeDatabaseBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemeDatabaseBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemeDatabaseBotApplication.class, args);
+	}
+}
